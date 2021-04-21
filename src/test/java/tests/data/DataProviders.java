14c7fbb797bac088952d9e4a8e1b36b8fed34af6
Imanuel Eyal <hodiya.eyal@gmail.com>
2021-04-21 12:35:53 +0300

    Adding the third test: send GET request to non-exist endpoints and verify status code

--- src/test/java/tests/data/DataProviders.java
+++ src/test/java/tests/data/DataProviders.java
@@ -6,10 +6,29 @@ import org.testng.annotations.DataProvider;
 
 public class DataProviders {
 
-	@DataProvider(name = "create")
-	public static Object[][] createData() throws IOException {
+	@DataProvider(name = "allValidEndpoints")
+	public static Object[][] getValidEndpoints() throws IOException {
 		
 		String endpointsArrayString = PropertiesFilesRetriever.getEndpointProperties().getProperty("endpoints");
+		
+		String[] endpointsArray = endpointsArrayString.split(",");
+		
+		int endpointsNumber = endpointsArray.length; 
+
+		Object[][] endpointsData = new Object[endpointsNumber][1];
+		
+		for (int index = 0; index < endpointsNumber; index++) {
+			endpointsData[index][0] = endpointsArray[index];
+		}
+		
+		return endpointsData;
+	}
+	
+	
+	@DataProvider(name = "unvalidEndpoints")
+	public static Object[][] getUnvalidEndpoints() throws IOException {
+		
+		String endpointsArrayString = PropertiesFilesRetriever.getEndpointProperties().getProperty("endpointsNotExist");
 		
 		String[] endpointsArray = endpointsArrayString.split(",");
 		
